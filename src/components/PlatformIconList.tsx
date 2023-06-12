21fc2229f82e38cf442a24a847a70c810b5fe3b9
suresh-02 <suresh2002infinity@gmail.com>
2023-06-12 13:13:54 +0530

    commit

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { Platform } from "../Hooks/useGames"
-import { HStack, Icon, Text } from "@chakra-ui/react"
+import { HStack, Icon} from "@chakra-ui/react"
 import {
     FaWindows, 
     FaPlaystation, 
